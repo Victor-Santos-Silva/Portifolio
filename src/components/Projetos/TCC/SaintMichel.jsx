b2bd0d1b049b2d01586ba76238240f15d2f1060e
Victor dos Santos <victorsantos.goal@gmail.com>
2025-08-01 07:54:54 -0300

    colocando uma descrição no projeto

--- src/components/Projetos/TCC/SaintMichel.jsx
+++ src/components/Projetos/TCC/SaintMichel.jsx
@@ -10,7 +10,7 @@ const SaintMichel = () => {
                 <h1>Hospital Saint Michel</h1>
                 <img src={imgSaintMichel} alt="imgSaintMichel" className='imgSaintMichel' />
                 <h2>Descrição:</h2>
-                <p>dsadadsadasd</p>
+                <p>Criado para um ambiente hospitalar, esse sistema resolve um dos maiores desafios da área da saúde: agendamentos confusos e demorados. Com ele, pacientes agendam consultas de forma prática e intuitiva, enquanto a equipe médica ganha mais controle e organização. (Projeto de TCC)</p>
                 <Link to='https://hospitalsaintmichel.vercel.app/' target='_blank'><b>Veja mais</b></Link>
             </section>
         </>
