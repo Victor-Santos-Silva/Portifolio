62905d625ee01a125de068bdac4549e4a05e93a9
Victor dos Santos <victorsantos.goal@gmail.com>
2025-08-13 21:01:33 -0300

    colocando novas tecnologias em experiencia

--- src/components/Experiencia/Experiencia.jsx
+++ src/components/Experiencia/Experiencia.jsx
@@ -5,9 +5,16 @@ import html from "../../assets/Html.png"
 import css from "../../assets/Css.png"
 import javaScript from "../../assets/JavaScript.png";
 import react from "../../assets/React.png";
+import Next from "../../assets/Next.png";
 import node from "../../assets/NodeJs.png";
 import java from "../../assets/Java.png"
 import mysql from "../../assets/MySQL.png";
+import postgress from "../../assets/Postgress SQL.png";
+import typescript from "../../assets/Typescript.png";
+import NestJS from "../../assets/NestJS.png";
+import MongoDB from "../../assets/MongoDB.png";
+import azure from "../../assets/Microsoft_Azure.png";
+import gcp from "../../assets/cgp cloud.png";
 
 const Experiencia = () => {
     return (
@@ -18,18 +25,37 @@ const Experiencia = () => {
                 <div className='icones-tecnologias'>
 
                     <div className='container-tecnologias'>
+                        <h1>Linguagens:</h1>
+                        <img src={javaScript} alt="icone JavaScript" className='imagem' />
+                        <img src={typescript} alt="icone Node Js" className='imagem' />
+                        <img src={java} alt="icone Node Js" className='imagem' />
+                    </div>
+
+                    <div className='container-tecnologias'>
                         <h1>Front End:</h1>
                         <img src={html} alt="icone MySQL" className='imagem' />
                         <img src={css} alt="icone MySQL" className='imagem' />
-                        <img src={javaScript} alt="icone JavaScript" className='imagem' />
                         <img src={react} alt="icone React" className='imagem' />
+                        <img src={Next} alt="icone React" className='imagem' />
                     </div>
 
                     <div className='container-tecnologias'>
                         <h1>Back End:</h1>
                         <img src={node} alt="icone Node Js" className='imagem' />
-                        {/* <img src={java} alt="icone MySQL" className='imagem' /> */}
+                        <img src={NestJS} alt="icone Node Js" className='imagem' />
+                    </div>
+
+                    <div className='container-tecnologias'>
+                        <h1>Banco de Dados:</h1>
                         <img src={mysql} alt="icone MySQL" className='imagem' />
+                        <img src={postgress} alt="icone MySQL" className='imagem' />
+                        <img src={MongoDB} alt="icone MySQL" className='imagem' />
+                    </div>
+                    
+                    <div className='container-tecnologias'>
+                        <h1>Cloud:</h1>
+                        <img src={azure} alt="icone MySQL" className='imagem' />
+                        <img src={gcp} alt="icone MySQL" className='imagem' />
                     </div>
                 </div>
             </div>
