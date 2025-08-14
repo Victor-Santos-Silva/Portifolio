0539d398eba75461a29df9f3304930c13c3f4b03
Victor dos Santos <victorsantos.goal@gmail.com>
2025-08-13 21:13:40 -0300

    colocando tecnologias nos projetos utilizados

--- src/components/Projetos/Overclock/Overclock.jsx
+++ src/components/Projetos/Overclock/Overclock.jsx
@@ -1,5 +1,7 @@
 import './overclock.css'
-
+import html from "../../../assets/Html.png"
+import css from "../../../assets/Css.png"
+import javaScript from "../../../assets/JavaScript.png";
 import imgOverclock from '../../../assets/HomePageOverclock.png'
 import { Link } from 'react-router-dom';
 
@@ -9,6 +11,10 @@ const Overclock = () => {
             <section className='container-overclock'>
                 <h1>Overclock</h1>
                 <img src={imgOverclock} alt="imgOverclock" className='imgOverclock' />
+                <h2>tecnologias usadas:</h2>
+                <img src={html} alt="icone JavaScript" className='imagem' />
+                <img src={css} alt="icone JavaScript" className='imagem' />
+                <img src={javaScript} alt="icone JavaScript" className='imagem' />
                 <h2>Descrição:</h2>
                 <p>Um "clone" do site da Overclock.</p>
                 <Link to='https://site-overclock-bebida-gamer.vercel.app/' target='_blank' className='buttonVejaMais'><b>Veja mais</b></Link>
