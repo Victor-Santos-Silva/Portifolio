a63f31ca8b69d42e376a405b99a09bfe8af2dcde
Victor dos Santos <victorsantos.goal@gmail.com>
2025-09-30 16:47:09 -0300

    criando Tecnologias & Ferramentas

--- src/components/Tecnologias/Tecnologias.jsx
+++ src/components/Tecnologias/Tecnologias.jsx
@@ -1,41 +1,59 @@
+// tecnologias.jsx
 import React from "react";
 import "./tecnologias.css";
 
-import html from "../../assets/Html.png";
-import css from "../../assets/Css.png";
-import javaScript from "../../assets/JavaScript.png";
-import react from "../../assets/React.png";
-import Next from "../../assets/Next.png";
-import flutter from "../../assets/flutter.png";
-import reactNative from "../../assets/react-Native (1).png";
-import node from "../../assets/NodeJs.png";
-import mysql from "../../assets/MySQL.png";
-import postgress from "../../assets/Postgress SQL.png";
-import typescript from "../../assets/Typescript.png";
-import azure from "../../assets/Microsoft_Azure.png";
+// Importações das imagens
+import htmlIcon from "../../assets/Html.png";
+import cssIcon from "../../assets/Css.png";
+import jsIcon from "../../assets/JavaScript.png";
+import reactIcon from "../../assets/React.png";
+import nextIcon from "../../assets/Next.png";
+import flutterIcon from "../../assets/flutter.png";
+import reactNativeIcon from "../../assets/react-Native (1).png";
+import nodeIcon from "../../assets/NodeJs.png";
+import mysqlIcon from "../../assets/MySQL.png";
+import postgresIcon from "../../assets/Postgress SQL.png";
+import tsIcon from "../../assets/Typescript.png";
+import azureIcon from "../../assets/Microsoft_Azure.png";
+
+// 1. Criamos um array com os dados das tecnologias
+const tecnologias = [
+  { nome: "HTML", icone: htmlIcon },
+  { nome: "CSS", icone: cssIcon },
+  { nome: "JavaScript", icone: jsIcon },
+  { nome: "TypeScript", icone: tsIcon },
+  { nome: "React", icone: reactIcon },
+  { nome: "Next.js", icone: nextIcon },
+  { nome: "Flutter", icone: flutterIcon },
+  { nome: "React Native", icone: reactNativeIcon },
+  { nome: "Node.js", icone: nodeIcon },
+  { nome: "MySQL", icone: mysqlIcon },
+  { nome: "PostgreSQL", icone: postgresIcon },
+  { nome: "Azure", icone: azureIcon },
+];
 
 export default function Tecnologias() {
   return (
-    <section className="testeSection">
-      <h1 className="testeasdasd">
+    <section className="tecnologias-section">
+      <h2 className="tecnologias-titulo">Tecnologias & Ferramentas</h2>
+      <p className="section-descricao">
         Atualmente, estou em busca de integrar uma equipe inovadora que valorize
         a criação de soluções digitais acessíveis e impactantes, unindo
         tecnologia e experiência do usuário.
-      </h1>
-      <br />
-      <div className="container-icones">
-        <img src={html} alt="html" className="imgHtml" />
-        <img src={css} alt="html" className="imgHtml" />
-        <img src={javaScript} alt="html" className="imgHtml" />
-        <img src={typescript} alt="html" className="imgHtml" />
-        <img src={react} alt="html" className="imgHtml" />
-        <img src={Next} alt="html" className="imgHtml" />
-        <img src={flutter} alt="html" className="imgHtml" />
-        <img src={reactNative} alt="html" className="imgHtml" />
-        <img src={node} alt="html" className="imgHtml" />
-        <img src={mysql} alt="html" className="imgHtml" />
-        <img src={postgress} alt="html" className="imgHtml" />
-        <img src={azure} alt="html" className="imgHtml" />
+      </p>
+      
+      {/* 2. Usamos .map() para renderizar os ícones dinamicamente */}
+      <div className="icones-container">
+        {tecnologias.map((tech) => (
+          <div key={tech.nome} className="icone-wrapper">
+            <img 
+              src={tech.icone} 
+              alt={`Ícone do ${tech.nome}`} 
+              className="icone-img" 
+            />
+            <span>{tech.nome}</span>
+          </div>
+        ))}
       </div>
     </section>
   );
