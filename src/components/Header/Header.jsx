1070d9a42fdd1f3f306cd3f4cef569540da9c68e
Victor dos Santos <victorsantos.goal@gmail.com>
2025-11-07 14:21:41 -0300

    arrumando header, tirando efeito sanduiche

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,6 @@ import { Link } from "react-router-dom";
 import "./header.css"; // Supondo que o CSS esteja nesse arquivo
 
 function Header() {
-  // Estado para controlar se o menu mobile está aberto ou fechado
-  const [menuAberto, setMenuAberto] = useState(false);
-
   return (
     <header className="containerHeader">
       <div className="logo">
@@ -14,42 +11,21 @@ function Header() {
         </Link>
       </div>
 
-      {/* O botão hambúrguer só aparece no mobile */}
-      <button
-        className="menu-hamburguer"
-        onClick={() => setMenuAberto(!menuAberto)}
-      >
-        {/* Lógica simples para mostrar ☰ ou X */}
-        {menuAberto ? "X" : "☰"}
-      </button>
-
       {/* Adiciona a classe 'menu-aberto' quando o estado for true */}
-      <nav className={`menu-principal ${menuAberto ? "menu-aberto" : ""}`}>
+      <nav className={`menu-principal`}>
         <ul>
           <li>
-            <Link
-              to="/"
-              className="buttonHeader"
-              onClick={() => setMenuAberto(false)}
-            >
+            <Link to="/" className="buttonHeader">
               Home
             </Link>
           </li>
           <li>
-            <Link
-              to="/aboutme"
-              className="buttonHeader"
-              onClick={() => setMenuAberto(false)}
-            >
+            <Link to="/aboutme" className="buttonHeader">
               Sobre mim
             </Link>
           </li>
           <li>
-            <Link
-              to="/projects"
-              className="buttonHeader"
-              onClick={() => setMenuAberto(false)}
-            >
+            <Link to="/projects" className="buttonHeader">
               Projetos
             </Link>
           </li>
